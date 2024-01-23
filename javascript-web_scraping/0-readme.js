55260e4d34f9edf5f857d1b7d8bc882e666c639d
jnieves2177 <jnieves3177@gmail.com>
2024-01-23 12:38:16 -0400

    change

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-let fp = process.argv[2];
-if (!fp) {
-  fp = 'notFound';
-}
-fs.readFile(fp, 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
